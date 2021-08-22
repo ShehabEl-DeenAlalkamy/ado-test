c251c3c062dfa71c880032e89c28e4fc0dc387aa
shehabeldeen-essam <shehabeldeen.essam@vodafone.com>
2021-08-22 14:29:19 +0200

    oh la la

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
-// adding some line for testing   
+// oh lala    
 const DB_URL = process.env.MONGODB_URL || "localhost";
 const MONGODB_PORT = 27017;
 
